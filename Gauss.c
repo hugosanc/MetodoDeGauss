1a637180cc1f8ccfaf8d8a6faed6162a7c633f1a
Hugo Santana <hugosc@id.uff.br>
2018-03-29 11:16:35 -0300

    Impressão da matriz

--- Gauss.c
+++ Gauss.c
@@ -12,9 +12,9 @@ double **ler( char *nome, int *dim)
 	i = fscanf(leitura,"%d",dim);
 
 	M = malloc( *dim *sizeof(double *));
-	for( i = 0 ; i < *dim ; i++ ) {
+	for( i = 0 ; i < *dim ; i++ )
 		M[i] = malloc((*dim+1)*sizeof(double) );
-	}    
+	    
 	i = j = 0;
 
 	while ( fscanf(leitura,"%d",&a) != EOF) 
@@ -30,11 +30,24 @@ double **ler( char *nome, int *dim)
 	return M;
 }
 
+void imprime( double **M, int dim)
+{
+	int i, j;
+	for(i=0;i<dim;i++)
+	{
+		for(j=0;j<dim+1;j++)
+			printf("%3g ",M[i][j]);
+			  
+		puts("");
+	}	
+}
+
 void main(int argc, char **argv)
 {
 	double **L;
 	int dim;
 
 	L = ler(argv[1],&dim);
+	imprime(L,dim);
    
 }
